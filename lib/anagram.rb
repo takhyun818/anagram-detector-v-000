8923ba0fd9a0e0a27e199c0103e1f6910726f0a0
Tak H Kim <takhyun818@gmail.com>
2018-11-16 01:34:23 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+
+  end
+
+  def match(word_list)
+    word_list.detect do |match_word|
+      match_word.split(" ").sort == @word.split(" ").sort
+    end
+  end
+end
